88ed909cd40ba1d365253435efbc2fda5614a423
will050710-commits <will050710@gmail.com>
2026-05-06 07:10:56 +0700

    Update import path for L11Test5Section3 component

--- frontend/src/app/L11-test5-section3/page.js
+++ frontend/src/app/L11-test5-section3/page.js
@@ -1,4 +1,4 @@
-import L11Test5Section3 from "../../components/bailam/L11-test5-section3";
+import L11Test5Section3 from "@/components/bailam/L11-test5-section3";
 
 export default function Page() {
   return <L11Test5Section3 />;
