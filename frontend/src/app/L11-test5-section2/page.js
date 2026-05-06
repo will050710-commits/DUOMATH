46e286eb21f556683c03dfc04b9567eba8d237f2
will050710-commits <will050710@gmail.com>
2026-05-06 07:10:28 +0700

    Update page.js

--- frontend/src/app/L11-test5-section2/page.js
+++ frontend/src/app/L11-test5-section2/page.js
@@ -1,4 +1,4 @@
-import L11Test5Section2 from "../../components/bailam/L11-test5-section2";
+import L11Test5Section2 from "@/components/bailam/L11-test5-section2";
 
 export default function Page() {
   return <L11Test5Section2 />;
